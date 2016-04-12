86b6e372ae846a219977e5b0d50803f4cc221528
Vasilewski Oleg <olegtr87@gmail.com>
2016-04-13 01:02:57 +0300

    new db model after checkpoint

--- exchanger_postgres_create.sql
+++ exchanger_postgres_create.sql
@@ -17,7 +17,7 @@ CREATE TABLE "user_credentials" (
 	"id" serial NOT NULL,
 	"login" character varying(20) NOT NULL UNIQUE,
 	"password" character varying(20) NOT NULL,
-	"role" int NOT NULL,
+	"role" character varying NOT NULL,
 	CONSTRAINT user_credentials_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -25,31 +25,27 @@ CREATE TABLE "user_credentials" (
 
 
 
-CREATE TABLE "registry_operations" (
+CREATE TABLE "transaction" (
 	"id" serial NOT NULL,
 	"user_id" int NOT NULL,
-	"number_operation" int NOT NULL,
 	"operation_id" int NOT NULL,
-	"course_in_id" int NOT NULL,
-	"course_out_id" int NOT NULL,
-	"summ_in" int NOT NULL,
-	"summ_out" int NOT NULL,
+	"exchange_rate_id" int NOT NULL,
+	"sum_in" int NOT NULL,
 	"date_operation" TIMESTAMP NOT NULL,
-	"number_registry" int NOT NULL UNIQUE,
-	CONSTRAINT registry_operations_pk PRIMARY KEY ("id")
+	CONSTRAINT transaction_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
 
 
-CREATE TABLE "course" (
+CREATE TABLE "exchange_rate" (
 	"id" serial NOT NULL,
-	"buy_course" int NOT NULL,
-	"sell_course" int NOT NULL,
 	"date_course" TIMESTAMP NOT NULL UNIQUE,
-	"currency_id" int NOT NULL,
-	CONSTRAINT course_pk PRIMARY KEY ("id")
+	"currency_id_from" int NOT NULL,
+	"currency_id_to" int NOT NULL,
+	"conversion" double NOT NULL,
+	CONSTRAINT exchange_rate_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -60,8 +56,18 @@ CREATE TABLE "operation" (
 	"id" serial NOT NULL,
 	"name" character varying NOT NULL UNIQUE,
 	"status_block" BOOLEAN NOT NULL,
-	"tax" double precision NOT NULL,
+	"tax" double NOT NULL,
 	CONSTRAINT operation_pk PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "balance" (
+	"id" serial NOT NULL,
+	"sum" int NOT NULL,
+	CONSTRAINT balance_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
@@ -78,42 +84,17 @@ CREATE TABLE "currency" (
 
 
 
-CREATE TABLE "check" (
-	"id" int NOT NULL,
-	"date_check" TIMESTAMP NOT NULL,
-	"number_registry_id" int NOT NULL UNIQUE,
-	CONSTRAINT check_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
-CREATE TABLE "balance" (
-	"id" serial NOT NULL,
-	"debit" int NOT NULL,
-	"credit" int NOT NULL,
-	"registry_operations_id" int NOT NULL,
-	CONSTRAINT balance_pk PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
-);
-
-
-
 ALTER TABLE "user_profile" ADD CONSTRAINT "user_profile_fk0" FOREIGN KEY ("id") REFERENCES "user_credentials"("id");
 
 
-ALTER TABLE "registry_operations" ADD CONSTRAINT "registry_operations_fk0" FOREIGN KEY ("user_id") REFERENCES "user_credentials"("id");
-ALTER TABLE "registry_operations" ADD CONSTRAINT "registry_operations_fk1" FOREIGN KEY ("operation_id") REFERENCES "operation"("id");
-ALTER TABLE "registry_operations" ADD CONSTRAINT "registry_operations_fk2" FOREIGN KEY ("course_in_id") REFERENCES "course"("id");
-ALTER TABLE "registry_operations" ADD CONSTRAINT "registry_operations_fk3" FOREIGN KEY ("course_out_id") REFERENCES "course"("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk0" FOREIGN KEY ("user_id") REFERENCES "user_credentials"("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk1" FOREIGN KEY ("operation_id") REFERENCES "operation"("id");
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fk2" FOREIGN KEY ("exchange_rate_id") REFERENCES "exchange_rate"("id");
 
-ALTER TABLE "course" ADD CONSTRAINT "course_fk0" FOREIGN KEY ("currency_id") REFERENCES "currency"("id");
+ALTER TABLE "exchange_rate" ADD CONSTRAINT "exchange_rate_fk0" FOREIGN KEY ("currency_id_from") REFERENCES "currency"("id");
+ALTER TABLE "exchange_rate" ADD CONSTRAINT "exchange_rate_fk1" FOREIGN KEY ("currency_id_to") REFERENCES "currency"("id");
 
 
 
-ALTER TABLE "check" ADD CONSTRAINT "check_fk0" FOREIGN KEY ("number_registry_id") REFERENCES "registry_operations"("number_registry");
-
-ALTER TABLE "balance" ADD CONSTRAINT "balance_fk0" FOREIGN KEY ("registry_operations_id") REFERENCES "registry_operations"("id");
+ALTER TABLE "currency" ADD CONSTRAINT "currency_fk0" FOREIGN KEY ("id") REFERENCES "balance"("id");
 
